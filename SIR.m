b3fd160edf73ec056501424f799308bdd1b10697
Toffanelly <1621227+Toffanelly@users.noreply.github.com>
2021-01-04 01:06:28 +0100

    Unito le due funzioni responsabili della simulazione SIR in SIR.m

--- SIR.m
+++ SIR.m
@@ -1,4 +1,16 @@
-function dYdt = SIR(t,Y,c,beta,gamma,N)
+function [t,S,I,R] = SIR(c,beta,gamma,S0,I0,R0,tmax)
+%Simulazione SIR a determinata condizione iniziale e parametri.
+tspan=[0 tmax];
+y0=[S0 I0 R0];
+N=S0+I0+R0;
+[t,y]=ode45(@(t,y) differentialSIR(t,y,c,beta,gamma,N), tspan, y0);
+plot(t,y)
+
+S=y(:,1);
+I=y(:,2);
+R=y(:,3);
+end
+function dYdt = differentialSIR(t,Y,c,beta,gamma,N)
 dYdt=zeros(3,1);
 S=Y(1);
 I=Y(2);
