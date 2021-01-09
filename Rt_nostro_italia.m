970c06525df7c35a51cac7e712718dd3ade20c49
piezga <76875435+piezga@users.noreply.github.com>
2021-01-09 12:59:44 +0100

    prima ho cannato

---
+++ Rt_nostro_italia.m
@@ -0,0 +1,13 @@
+function Rt_nostro_italia = Rt(totale_positivi,gamma)
+
+load('.\data\andamento-nazionale.mat')
+gamma = 1/9;
+
+dlnI=diff(log(andamento_nazionale.totale_positivi));
+Rt_nostro_italia=(dlnI + gamma)/gamma;
+plot(weekAverage(Rt_nostro_italia))
+title('R_t come derivata logaritmica (IT)')
+xlabel("Settimane dall'inizio")
+ylabel("R_t")
+
+end
