bda399840b2d95b8cf4a28cad1a15b593b861329
piezga <76875435+piezga@users.noreply.github.com>
2021-01-13 01:13:41 +0100

    fixato tutto il fixabile

---
+++ confinterval.m
@@ -0,0 +1,11 @@
+function [low,high] = confinterval(data,domain,prob)
+    totalprob = cumsum(data) - cumsum(data)';
+    [highs,lows] = find(totalprob>prob);
+    [best, bestindex] = min(highs-lows);
+    bestcouple = [highs(bestindex),lows(bestindex)];
+    high = domain(bestcouple(1))
+    low = domain(bestcouple(2))
+end
+    
+    
+    
